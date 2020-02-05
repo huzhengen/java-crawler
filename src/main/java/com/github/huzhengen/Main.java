d5a5d3a60423be09850319b5a23444a4f7ecdb83
hukai <huzhengen@163.com>
2020-02-05 16:57:40 +0800

    删除注释

--- src/main/java/com/github/huzhengen/Main.java
+++ src/main/java/com/github/huzhengen/Main.java
@@ -13,12 +13,9 @@ public class Main {
     public static void main(String[] args) throws IOException {
         CloseableHttpClient httpclient = HttpClients.createDefault();
         HttpGet httpGet = new HttpGet("https://sina.cn/");
-
         try (CloseableHttpResponse response1 = httpclient.execute(httpGet)) {
             System.out.println(response1.getStatusLine());
             HttpEntity entity1 = response1.getEntity();
-            // do something useful with the response body
-            // and ensure it is fully consumed
             System.out.println(EntityUtils.toString(entity1));
         }
     }
